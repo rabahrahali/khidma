ddd801252e3d48bf7e536d18bd094c57b6b7d64c
adel-mgb <85323801+adel-mgb@users.noreply.github.com>
2021-10-15 18:00:55 +0100

    Add files via upload

--- khidma.js
+++ khidma.js
@@ -1,1 +1,10 @@
-
+/*hello world*/
+function backGround1(){
+     var x=document.getElementById('body');
+     
+         x.style.color = 'blue' ;
+}
+function backGround0(){
+    var x=document.getElementById('body');
+    x.style.color = 'black';
+}
