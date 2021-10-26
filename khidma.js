ae05be426be7fb69e07656f17419bd7bf60f67a9
alizhassane <hassanealiz11@gmail.com>
2021-10-26 18:27:13 +0100

    Update khidma.js

--- khidma.js
+++ khidma.js
@@ -1,10 +1,11 @@
 /*hello world*/
-function backGround1(){
-     var x=document.getElementById('body');
-     
-         x.style.color = 'blue' ;
+function backGround1() {
+    var x = document.getElementById('body');
+
+    x.style.color = 'blue';
 }
-function backGround0(){
-    var x=document.getElementById('body');
+
+function backGround0() {
+    var x = document.getElementById('body');
     x.style.color = 'black';
 }
